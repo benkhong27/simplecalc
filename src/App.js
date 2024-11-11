5289c6f7ca79e736609229d2e3f078610e42f281
benkhong27 <benkhong27@gmail.com>
2024-11-10 18:50:01 -0800

    added buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
         <div className="previous-operand"></div>
         <div className="current-operand"></div>
       </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
     </div>
   );
 }
