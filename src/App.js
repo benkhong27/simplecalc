bcabdb1ded05941d7bfeb3868ae58dab63923b1a
benkhong27 <benkhong27@gmail.com>
2024-11-10 18:45:34 -0800

    added output

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return null;
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
